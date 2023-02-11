c2ccd5fe895baaa05005aa825a57cfd94646b71c
Basem Elsisy <basemsisy@gmail.com>
2023-02-11 15:17:18 +0200

    feat: implement sidebar component

---
+++ src/components/molecules/Sidebar/Sidebar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ListItem from "src/components/atoms/ListItem/ListItem";
+import Logo from "src/components/atoms/Logo/Logo";
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar flex flex-col h-[100%] fixed w-[248px] bg-white py-[30px] px-6 pr-[50px]">
+      <Logo />
+      <div className="list-items flex flex-col flex-1 justify-between">
+        <ul className="top-list">
+          <ListItem text="dashboard" icon="dashboard" />
+          <ListItem text="cars" icon="car" />
+        </ul>
+        <ul className="bottom-list bottom-0">
+          <ListItem text="settings" icon="settings" />
+          <ListItem text="log out" icon="sign-out" />
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
