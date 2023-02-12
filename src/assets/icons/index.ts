d518ca19d7c454df96b19f91a43a29661e5907fd
Basem Elsisy <basemsisy@gmail.com>
2023-02-13 01:25:30 +0200

    feat: add icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,7 @@
 import { ReactComponent as EnergyIcon } from "./energy.svg";
+import { ReactComponent as RotationIcon } from "./rotation.svg";
+import { ReactComponent as IconOne } from "./icon-1.svg";
+import { ReactComponent as IconTwo } from "./icon-2.svg";
+import { ReactComponent as ElectricIcon } from "./electric.svg";
 
-export { EnergyIcon };
+export { EnergyIcon, RotationIcon, IconOne, IconTwo, ElectricIcon };
